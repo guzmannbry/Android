e0a16941c1e2bb7e105bf46a4f3607e8cec5c41b
guzmannbry <g.bryan95@yahoo.com>
2017-04-28 06:58:41 -0500

    Add files via upload

---
+++ StudInfo/src/com/example/studinfo/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.studinfo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener, OnItemSelectedListener {
+
+	EditText txtidno,txtName;
+	Spinner cboCourse;
+	RadioGroup gender;
+	Button btnOk;
+	private String selected_course;
+	private String selected_gender;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    
+    this.txtidno=(EditText)this.findViewById(R.id.editText1);
+    this.txtName=(EditText)this.findViewById(R.id.editText2);
+    this.cboCourse=(Spinner)this.findViewById(R.id.spinner1);
+    this.gender=(RadioGroup)this.findViewById(R.id.radioGroup1);
+    this.btnOk=(Button)this.findViewById(R.id.button1);
+    
+    this.btnOk.setOnClickListener(this);
+    
+    this.cboCourse.setOnItemSelectedListener(this);
+  
+    }
+
+	@Override
+	public void onClick(View arg0) {
+		String idno=this.txtidno.getText().toString();
+		String name=this.txtName.getText().toString();
+		int genderindex =gender.getCheckedRadioButtonId();
+    	RadioButton btnGender = (RadioButton) this.findViewById(genderindex);
+    selected_gender=btnGender.getText().toString();
+    	
+		if(!idno.equals("")&&!name.equals("")){
+
+			String message="IDNO: "+idno+"\n"+ 
+					"NAME: "+name+"\n"+ 
+					"COURSE: "+selected_course+"\n"+ 
+					"GENDER: "+selected_gender; 
+				
+			
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Student Information");
+			builder.setMessage(message);
+			builder.setNeutralButton("Ok",null);
+			
+			AlertDialog dialog =builder.create();
+			dialog.show();
+		}
+		else{
+			Toast.makeText(this, "Fill All Fields",Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	@Override 
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		selected_course=this.cboCourse.getItemAtPosition(arg2).toString();
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+   
+    
+}
